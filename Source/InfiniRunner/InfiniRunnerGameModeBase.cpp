04b78dc986a9de68c7e7e73b4b9a1475fbdd5b45
Tor Nordmark <30878484+Play4leftovers@users.noreply.github.com>
2023-04-10 16:28:58 +0200

    Initial Commit

---
+++ Source/InfiniRunner/InfiniRunnerGameModeBase.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "InfiniRunnerGameModeBase.h"
+
+AInfiniRunnerGameModeBase::AInfiniRunnerGameModeBase()
+{
+	DefaultPawnClass = RunnerCharacter_BP_C::StaticClass();
+}
---
+++ Source/InfiniRunner/InfiniRunnerGameModeBase.h
@@ -0,0 +1,20 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "InfiniRunnerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INFINIRUNNER_API AInfiniRunnerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+public:
+	
+private:
+	
+};
